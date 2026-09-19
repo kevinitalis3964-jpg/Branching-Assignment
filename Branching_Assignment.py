1253e9204688eb13d25d384284a3ea8e2034b3de
kevinitalis3964-jpg <kevinitalis3964@gmail.com>
2026-09-18 22:40:53 -0400

    Add files via upload

---
+++ Branching_Assignment.py
@@ -0,0 +1,8 @@
+kw_hours = int(input("Enter the KW hours used: "))
+
+if kw_hours <= 1000:
+    amount_owed = kw_hours * 0.07633
+else:
+    amount_owed = (1000 * 0.07633) + ((kw_hours - 1000) * 0.09259)
+
+print("Amount owed is $", amount_owed, sep="")
